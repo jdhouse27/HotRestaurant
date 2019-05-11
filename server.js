a267113a2af3675b0a1762fe8701a8f0c2fd8f1c
boffman <bkhoffman@gmail.com>
2019-05-11 11:16:11 -0500

    updated a couple comments

--- server.js
+++ server.js
@@ -35,13 +35,13 @@ app.get("/api/tables", function(req, res) {
     return res.json(reservations);
   });
 
-// Create New Characters - takes in JSON input
+// Create New Reservations - takes in JSON input
 app.post("/api/reservations", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newReservation = req.body;
   
-    // Using a RegEx Pattern to remove spaces from newCharacter
+    // Using a RegEx Pattern to remove spaces from newReservation
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     newReservation.unique = newReservation.name.replace(/\s+/g, "").toLowerCase();
   
